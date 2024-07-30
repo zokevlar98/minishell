e06e29ef7ca857f66118dc17e283bbc707654acd
Mohamed Mazouz <mohmazou@e3r1p15.1337.ma>
2024-07-31 00:05:11 +0100

    cmd_struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mohmazou <mohmazou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 23:15:58 by mohmazou          #+#    #+#             */
-/*   Updated: 2024/07/30 23:40:10 by mohmazou         ###   ########.fr       */
+/*   Updated: 2024/07/31 00:02:25 by mohmazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,18 @@ typedef struct s_env
     char	*value;
     struct s_env	*next;
 }	t_env;
+
+// command linked list
+typedef struct s_cmd
+{
+	char			*ful_cmd;
+    int				pipe_line;
+    char			*cmd;
+    char			**args;
+    char			*in_redir;
+    char			*out_redir;
+    struct s_cmd	*next;
+}    t_cmd;
 
 // lib_utils_1.c
 int ft_strcmp(const char *s1, const char *s2);
