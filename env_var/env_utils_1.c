01a8c6146b3f7c81be1fe93ff204cf49eb671f74
Mohamed Mazouz <mohmazou@e3r1p15.1337.ma>
2024-07-31 00:49:10 +0100

    ft_env_add_back done

--- env_var/env_utils_1.c
+++ env_var/env_utils_1.c
@@ -6,7 +6,7 @@
 /*   By: mohmazou <mohmazou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 23:39:06 by mohmazou          #+#    #+#             */
-/*   Updated: 2024/07/31 00:38:04 by mohmazou         ###   ########.fr       */
+/*   Updated: 2024/07/31 00:45:10 by mohmazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,21 @@ static t_env	*ft_env_new(char *env)
 	return (new);
 }
 
+static void	ft_env_add_back(t_env **env_list, t_env *new)
+{
+	t_env	*tmp;
+
+	if (!*env_list)
+	{
+		*env_list = new;
+		return ;
+	}
+	tmp = *env_list;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+
 void	ft_env_list(t_env **env_list,char **env)
 {
 	int		i;
@@ -41,8 +56,7 @@ void	ft_env_list(t_env **env_list,char **env)
 	while (env[i])
 	{
 		new = ft_env_new(env[i]);
-		// ft_env_add_back(env_list, new);
+		ft_env_add_back(env_list, new);
 		i++;
 	}
-	printf("etst\n");
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohmazou <mohmazou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 23:17:58 by mohmazou          #+#    #+#             */
-/*   Updated: 2024/07/31 00:16:44 by mohmazou         ###   ########.fr       */
+/*   Updated: 2024/07/31 00:48:21 by mohmazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@ int main(int ac, char **av, char **env)
 	char *line;
 	t_env *env_list;
 
-	(void)env;
+	// initialize the env_list	
 	env_list = NULL;
 	ft_env_list(&env_list, env);
+	
+	
 	if (ac != 1 || av[1])
 	{
 		printf("Usage: %s\n", av[0]);
 		return (1);
 	}
+	// main loop
 	while (1)
 	{
 		line = readline("miishell$>  ");
