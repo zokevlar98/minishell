e2be7edab9528b1f6493a09471f22c322a5aec3b
zokevlar98 <zqouri11@gmail.com>
2024-08-11 08:53:32 +0100

    more about builtin cd

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 03:50:43 by zqouri            #+#    #+#             */
-/*   Updated: 2024/08/11 03:53:43 by zqouri           ###   ########.fr       */
+/*   Updated: 2024/08/11 08:19:43 by zqouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,63 @@ char	*get_home(t_env *env)
 {
 	t_env	*tmp;
 
-	if (env)
+	tmp = env;
+	if (tmp)
 	{
-		
+		while (tmp)
+		{
+			if (ft_strncmp(tmp->name, "HOME", 5) == 0)
+				return (tmp->value);
+			tmp = tmp->next;
+		}		
+	}
+	return (NULL);
+}
+
+void	cd_error(char *path, int flag)
+{
+	if (flag == 0)
+	{
+		ft_putstr_fd("cd: error retrieving current directory: getcwd: cannot ", 2);
+		ft_putstr_fd("access parent directories: No such file or directory\n", 2);	
+	}
+	else if (flag == 1 && path)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
 	}
 }
 
-void	ft_cd(t_cmd *cmd_list, t_env *env_list)
+void	ft_cd(t_cmd *cmd, t_env *env)
 {
 	char	*path;
+	char	*old_pwd;
 
-	if (cmd_list->args[1] == NULL)
+	old_pwd = getcwd(NULL, 0);
+	if (!old_pwd)
+		cd_error(NULL, 0);
+	if (!cmd->args[1] || !ft_strcmp(cmd->args[1], "~"))
 	{
-		// grep home in env list
-		if (chdir() == -1)
-			ft_error("no such file or directory\n");
+		path = get_home(env);
+		if ((chdir(path) == -1 && !cmd->args[1]) || !path)
+			ft_error("minishell: cd: HOME not set\n");
 	}
-	else if (chdir((const char *)cmd_list->args[1]) == -1)
-		ft_error("no such file or directory\n");
+	else if (!ft_strcmp(cmd->args[1], ".") || !ft_strcmp(cmd->args[1], ".."))
+	{
+		path = getcwd(NULL, 0);
+		if (chdir((const char *)cmd->args[1]) == -1 || !path)
+			cd_error(cmd->args[1], 0);
+		free(path);
+	}
+	else if (chdir((const char *)cmd->args[1]) == -1)
+		cd_error(cmd->args[1], 1);
+	//check if args[2] case if in exec if not the case in pars
+	//more check
 	path = getcwd(NULL, 0);
 	if (!path)
-		ft_error("no such file or directory\n");
-	ft_change_env(env_list, "PWD", path);
+		cd_error(NULL, 0);
+	ft_change_env(env, "OLDPWD", old_pwd);
+	ft_change_env(env, "PWD", path);
 	free(path);
 }
