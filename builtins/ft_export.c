10b27178842151ebabbc7c6cebe794d484b7f844
zokevlar98 <zqouri11@gmail.com>
2024-08-13 15:45:26 +0100

    export work Tue Aug 13 15:45:26 +01 2024

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: zqouri < zqouri@student.1337.ma >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/11 09:00:37 by zqouri            #+#    #+#             */
-/*   Updated: 2024/08/12 04:53:16 by zqouri           ###   ########.fr       */
+/*   Created: 2024/08/13 15:38:28 by zqouri            #+#    #+#             */
+/*   Updated: 2024/08/13 15:38:35 by zqouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	print_list_declare(t_env *env)
 {
 	while (env)
 	{
+		// I need to print with alhabetical order
 		printf("declare -x %s=\"%s\"\n", env->name, env->value);
 		env = env->next;
 	}
@@ -24,10 +25,12 @@ void	print_list_declare(t_env *env)
 void	ft_export(t_cmd *cmd, t_env *env)
 {
 	t_env	*tmp;
+	t_env	*p;
 	t_env	*new;
 	char	**str;
 
 	tmp = env;
+	p = env;
 	if (!cmd->args[1])
 	{
 		print_list_declare(tmp);
@@ -38,8 +41,9 @@ void	ft_export(t_cmd *cmd, t_env *env)
 		if (ft_strncmp(tmp->name, cmd->args[1], ft_strlen(tmp->name)) == 0)
 		{
 			str = ft_split(cmd->args[1], '=');
-			affiche_node(cmd);
-			ft_change_env(env, str[1], str[2]);
+			// affiche_env(env);
+			ft_change_env(p, tmp->name, str[1]);
+			// affiche_env(env);
 			return ;
 		}
 		tmp = tmp->next;
@@ -50,7 +54,7 @@ void	ft_export(t_cmd *cmd, t_env *env)
 		new = ft_env_new_(str[0], str[1]);
 		if (!new)
 			ft_error("error: feailed");
-		ft_env_add_back(&tmp, new);
+		ft_env_add_back(&env, new);
 		return ;
 	}
 	else
