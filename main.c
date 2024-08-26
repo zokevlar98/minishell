904117903c8d7e2cf61d0f35cd5ef386af6c161e
zokevlar98 <zqouri11@gmail.com>
2024-08-26 09:52:33 +0100

    fix after merge

--- main.c
+++ main.c
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/13 15:39:16 by zqouri            #+#    #+#             */
-/*   Updated: 2024/08/26 01:45:55 by zqouri           ###   ########.fr       */
-
-/*   Created: 2024/07/30 23:17:58 by mohmazou          #+#    #+#             */
-/*   Updated: 2024/07/31 01:31:42 by mohmazou         ###   ########.fr       */
-
+/*   Created: 2024/08/26 09:51:55 by zqouri            #+#    #+#             */
+/*   Updated: 2024/08/26 09:51:57 by zqouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +18,12 @@ int main(int ac, char **av, char **env)
 {
 	char *line;
 	t_env *env_list;
-	t_cmd *cmd_list;
-
 
 	env_list = NULL;
 	ft_env_list(&env_list, env);
 	t_cmd *cmd_list = (t_cmd *)malloc(sizeof(t_cmd));
-	cmd_list = NULL;
+	if (!cmd_list)
+		ft_error("allocation failed");
 	// check for arguments
 	if (ac != 1 || av[1])
 	{
