61d327661e2c8ca908ba6f3ec84ffc94b6a6b268
zokevlar98 <zqouri11@gmail.com>
2024-09-27 20:05:02 +0100

    GB Fri Sep 27 20:05:02 +01 2024

---
+++ gb/garbage.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   garbage.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 09:42:43 by zqouri            #+#    #+#             */
+/*   Updated: 2024/09/27 09:42:53 by zqouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GARBAGE_H
+
+# define GARBAGE_H
+
+# include <stdlib.h>
+
+typedef struct s_garb
+{
+	void			*addr;
+	struct s_garb	*next;
+}				t_garb ;
+
+void	clear_list(t_garb *list);
+void	add_node(t_garb **list, t_garb *node);
+void	*gb_malloc(size_t size, int type);
+
+#endif
